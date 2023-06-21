cb57b29ecf32b0c33f1f89e7eb8fc728b07545c5
signo <you@example.com>
2023-06-21 15:21:10 +0200

    function insert created

--- routes/session.js
+++ routes/session.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const sessionRouter = express.Router();
 const { createNewSession } = require('../utils/session');
+const prisma = require('../utils/db');
+
 
 sessionRouter.post('/create', async (req, res) => {
     const { id1 } = req.body;
     const newSession = await createNewSession(id1);
     res.json(newSession);
+});
+sessionRouter.post('/insert', async (req, res) => {
+    const { player, session, cell } = req.body;
+    res.json(await prisma.session.update({
+        where: {
+            id: session,
+        },
+        data: {
+            [cell]: player,
+        }
+    }));
 });
 
 module.exports = sessionRouter;
