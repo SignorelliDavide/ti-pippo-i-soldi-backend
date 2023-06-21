4f2cb0f8a8ae76b6fd8423a1c07947729613b6d0
signo <you@example.com>
2023-06-21 15:20:31 +0200

    function create adjusted

--- utils/session.js
+++ utils/session.js
@@ -4,13 +4,12 @@ const prisma = new PrismaClient();
 
 async function createNewSession(id1) {
     try {
-        const newSession = await prisma.user.create({
+        const newSession = await prisma.session.create({
             data: {
                 user1Id: id1,
             },
         });
         console.log('New session created');
-        return newUser;
     } catch (error) {
         console.error('Error during the creation of the session:', error);
     } finally {
