f0ac67377aff116d3b84de0faf4aa0601961fefe
signo <you@example.com>
2023-09-11 11:39:10 +0200

    findbyuser

--- utils/session.js
+++ utils/session.js
@@ -15,7 +15,25 @@ async function createNewSession(id1) {
         console.error('Error during the creation of the session:', error);
     }
 }
+async function findByUser(id) {
+    const found = await prisma.session.findMany({
+        where: {
+            user1Id: id,
+        },
+    });
+    console.log(found)
+    if (found.length == 0) {
+        const found2 = await prisma.session.findMany({
+            where: {
+                user2Id: id,
+            },
+        });
+        return found2;
+    }
+    return found;
+}
 
 module.exports = {
-    createNewSession
+    createNewSession,
+    findByUser
 };
